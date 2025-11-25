0d69be51354599a38d2c5ac9229f2a80ca969c90
caglardev <caglarcercinli@outlook.com>
2025-11-25 11:11:46 +0100

    resumepoint field resolver

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -3,14 +3,15 @@ import { Book } from "./models";
 export const resolvers = {
   Query: {
     favoriteBooks: async (_, __, { dataSources }): Promise<Book> => {
-      const result = (await dataSources.bookAPI.getFavorites()).entries;
-      const resumepoint = (await dataSources.resumepointAPI.getResumePoint())
-        .resumepoint;
-      // TODO: add mapping to resumepoint
-      result.forEach((book) => {
-        book.resumepoint = resumepoint;
-      });
-      return result;
+      return (await dataSources.bookAPI.getFavorites()).entries;
+    },
+  },
+  Book: {
+    resumepoint: async (parent, __, { dataSources }, info) => {
+      const resumepointData = (
+        await dataSources.resumepointAPI.getResumePoint()
+      ).resumepoint;
+      return resumepointData;
     },
   },
 };
